cbad082bfdab1bd5579def92c36911c2041af47b
Viplovesana <viplovesana90@gmail.com>
2025-04-16 02:04:58 +0530

    new commit

--- src/comp5.jsx
+++ src/comp5.jsx
@@ -1,11 +1,14 @@
 
+import { mycontext } from "./App";
+import { useContext } from "react";
 
 
 const Comp5=()=>{
+    const {user}=useContext(mycontext)
     return(
         <>
         <h1>component 5!</h1>
-        <h1>welcome!!!!</h1>
+        <h1>welcome!!!! {user}</h1>
         </>
     )
 }
