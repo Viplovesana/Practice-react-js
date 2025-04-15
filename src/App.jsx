fa099c4c1fd2d2061c30a39d9a0d4ea3ecd1c4ac
Viplovesana <viplovesana90@gmail.com>
2025-04-16 01:28:30 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,63 @@
 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Insert from "./pages/insert";
-import Display from "./pages/Display";
-import Contact from "./pages/contact";
-import Update  from "./pages/update";
-import Search from "./pages/search";
-import MyEdit from "./pages/MyEdit";
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Insert from "./pages/insert";
+// import Display from "./pages/Display";
+// import Contact from "./pages/contact";
+// import Update  from "./pages/update";
+// import Search from "./pages/search";
+// import MyEdit from "./pages/MyEdit";
 
-const App=()=>{
-    return(
-        <>
-
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/insert" element={<Insert/>}/>
-            <Route path="/display" element={<Display/>}/>
-            <Route path="/search" element={<Search/>}/>
-            <Route path="/update" element={<Update/>}/>
-            <Route path="/myedit/:id" element={<MyEdit/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            
-
-            </Route>
-        </Routes>
-        </BrowserRouter>
-        
-        
-        </>
-    )
-};
-export default App;
-
-
-
-// import { useState } from "react";
-// import Comp1 from "./comp1";
 // const App=()=>{
-// const [user,setUser]=useState("viplove")
-
 //     return(
 //         <>
-//         <h1>welcome!!!  {user}</h1>
-//         <Comp1 />
+
+//         <BrowserRouter>
+//         <Routes>
+//             <Route path="/" element={<Layout/>}>
+//             <Route index element={<Home/>}/>
+//             <Route path="/home" element={<Home/>}/>
+//             <Route path="/insert" element={<Insert/>}/>
+//             <Route path="/display" element={<Display/>}/>
+//             <Route path="/search" element={<Search/>}/>
+//             <Route path="/update" element={<Update/>}/>
+//             <Route path="/myedit/:id" element={<MyEdit/>}/>
+//             <Route path="/contact" element={<Contact/>}/>
+            
+
+//             </Route>
+//         </Routes>
+//         </BrowserRouter>
+        
         
 //         </>
 //     )
-// }
+// };
 // export default App;
 
+
+
+import { useState ,createContext} from "react";
+import Comp1 from "./comp1";
+const mycontext=createContext();
+const App=()=>{
+const [user,setUser]=useState("viplove!")
+
+    return(
+        <>
+        <h1>welcome!!!  {user}</h1>
+        <mycontext.Provider value={{user}}>
+        <Comp1 />
+        </mycontext.Provider>
+       
+        
+        </>
+    )
+}
+export default App;
+export {mycontext}                               
+ 
 
 
 
--- src/comp3.jsx
+++ src/comp3.jsx
@@ -1,10 +1,13 @@
 
 import Comp4 from "./comp4";
-
+import { useContext } from "react";
+import { mycontext } from "./App";
+    
 const Comp3=()=>{
+    const{user}=useContext(mycontext)
     return(
         <>
-        <h1>component 3!</h1>
+        <h1>component 3! {user}</h1>
         <Comp4 />
         </>
     )
